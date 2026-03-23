11090fe5714412099c60c518bdd94a7ba5d339fe
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-03-23 16:14:19 +0700

    fix: revert isReady gate on blog pages (causes SDK deadlock)

--- web/stdb/blog/src/app/[id]/page.tsx
+++ web/stdb/blog/src/app/[id]/page.tsx
@@ -1,20 +1,13 @@
 'use client'
 import { tables } from '@a/be-spacetimedb/spacetimedb'
 import { parseId } from '@a/fe/utils'
-import { Spinner } from '@a/ui/spinner'
 import { useParams } from 'next/navigation'
 import { useTable } from 'spacetimedb/react'
 import Client from './client'
 const Page = () => {
   const { id: raw } = useParams<{ id: string }>(),
     id = parseId(raw),
-    [blogs, isReady] = useTable(tables.blog)
-  if (!isReady)
-    return (
-      <div className='flex min-h-40 items-center justify-center'>
-        <Spinner />
-      </div>
-    )
+    [blogs] = useTable(tables.blog)
   return <Client blog={id === null ? null : (blogs.find(b => b.id === id) ?? null)} />
 }
 export default Page
