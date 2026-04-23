737e82cc2da0f9371bbb4147d603e11490aeb3ab
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-24 00:53:36 +0700

    feat(noboil): export log/kv/quota factories + table helpers from server/index

--- lib/noboil/src/convex/server/index.ts
+++ lib/noboil/src/convex/server/index.ts
@@ -22,20 +22,26 @@ export {
   ok,
   time
 } from './helpers'
+export { makeKv } from './kv'
+export { makeLog } from './log'
 export { auditLog, composeMiddleware, inputSanitize, slowQueryWarn } from './middleware'
 export { noboil } from './noboil'
 export { makeOrg } from './org'
 export type { InviteDocLike, JoinRequestItem, OrgDocLike, OrgMemberItem, OrgUserLike } from './org'
 export { canEdit, getOrgMember, getOrgRole, makeOrgCrud, orgCascade, requireOrgMember, requireOrgRole } from './org-crud'
 export { HEARTBEAT_INTERVAL_MS, makePresence, PRESENCE_TTL_MS, presenceTable } from './presence'
+export { makeQuota } from './quota'
 export {
   baseTable,
   checkSchema,
   childTable,
+  kvTable,
+  logTable,
   orgChildTable,
   orgTable,
   orgTables,
   ownedTable,
+  quotaTable,
   rateLimitTable,
   singletonTable,
   uploadTables
